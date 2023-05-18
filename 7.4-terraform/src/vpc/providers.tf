473e2a06addcf746c25ca705bf093dc12a5debb3
Yagudin Timur <yagudin123@gmail.com>
2023-05-18 22:34:33 +0300

    ter-PR

--- 7.4-terraform/src/vpc/providers.tf
+++ 7.4-terraform/src/vpc/providers.tf
@@ -2,7 +2,15 @@ terraform {
   required_providers {
     yandex = {
       source = "yandex-cloud/yandex"
+      version = ">= 0.7"
     }
   }
   required_version = ">=0.13"
+}
+
+provider "yandex" {
+  token     = var.token
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+
 }
